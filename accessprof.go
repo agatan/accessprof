475dc312811d9c3cdf837e9b5fb5081902e4e98d
agatan <agatan039@gmail.com>
2017-11-23 17:02:04 +0900

    Report aggeregation

--- accessprof.go
+++ accessprof.go
@@ -6,6 +6,12 @@ import (
 	"time"
 )
 
+var defaultAccessProf AccessProf
+
+func Wrap(h http.Handler) http.Handler {
+	return defaultAccessProf.Wrap(h)
+}
+
 type AccessLog struct {
 	Method           string
 	Path             string
@@ -43,8 +49,45 @@ func (a *AccessProf) Count() int {
 	return n
 }
 
-var defaultAccessProf AccessProf
+func (a *AccessProf) Report() *Report {
+	var segs []*ReportSegment
+	a.mu.Lock()
+	defer a.mu.Unlock()
 
-func Wrap(h http.Handler) http.Handler {
-	return defaultAccessProf.Wrap(h)
+	for _, l := range a.accessLogs {
+		for _, seg := range segs {
+			if seg.match(l) {
+				seg.add(l)
+				goto next
+			}
+		}
+		segs = append(segs, &ReportSegment{
+			Method:     l.Method,
+			Path:       l.Path,
+			Status:     l.Status,
+			AccessLogs: []*AccessLog{l},
+		})
+	next:
+	}
+
+	return &Report{Segments: segs}
+}
+
+type ReportSegment struct {
+	Method     string
+	Path       string
+	Status     int
+	AccessLogs []*AccessLog
+}
+
+func (seg *ReportSegment) match(l *AccessLog) bool {
+	return seg.Method == l.Method && seg.Path == l.Path && seg.Status == l.Status
+}
+
+func (seg *ReportSegment) add(l *AccessLog) {
+	seg.AccessLogs = append(seg.AccessLogs, l)
+}
+
+type Report struct {
+	Segments []*ReportSegment
 }
--- accessprof_test.go
+++ accessprof_test.go
@@ -25,3 +25,17 @@ func TestAccessProf_Wrap_recordsRequests(t *testing.T) {
 		t.Fatalf("The server got 3 requests, but got %d access logs", accessProf.Count())
 	}
 }
+
+func TestAccessProf_Report_aggregatesByMethod(t *testing.T) {
+	var a AccessProf
+	server := httptest.NewServer(a.Wrap(testHandler))
+	defer server.Close()
+
+	http.Get(server.URL)
+	http.Post(server.URL, "", nil)
+
+	report := a.Report()
+	if len(report.Segments) != 2 {
+		t.Fatalf("expected 2 report segments, GET / and POST /; but got %d", len(report.Segments))
+	}
+}
