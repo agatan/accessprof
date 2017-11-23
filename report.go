7de7f0dcfa03a7cea6b46452b411b7f6e16d7abe
agatan <agatan039@gmail.com>
2017-11-23 19:21:42 +0900

    Report as HTML

--- report.go
+++ report.go
@@ -2,11 +2,13 @@ package accessprof
 
 import (
 	"bytes"
+	"io"
 	"math"
 	"regexp"
 	"strconv"
 	"time"
 
+	"github.com/alecthomas/template"
 	"github.com/olekukonko/tablewriter"
 )
 
@@ -134,3 +136,65 @@ func (r *Report) String() string {
 	w.Render()
 	return buf.String()
 }
+
+func (r *Report) RenderHTML(w io.Writer) error {
+	data := struct {
+		Header []string
+		Rows   [][]string
+	}{}
+	data.Header = []string{"STATUS", "METHOD", "PATH", "COUNT", "MIN", "MAX", "SUM", "AVG", "MIN(BODY)", "MAX(BODY)", "Sum(body)", "AVG(BODY)"}
+	for _, seg := range r.Segments {
+		data.Rows = append(data.Rows, []string{
+			strconv.Itoa(seg.Status),
+			seg.Method,
+			seg.AggregationPath(),
+			strconv.Itoa(seg.Count()),
+			seg.MinResponseTime().String(),
+			seg.MaxResponseTime().String(),
+			seg.SumResponseTime().String(),
+			seg.AvgResponseTime().String(),
+			strconv.Itoa(seg.MinBody()),
+			strconv.Itoa(seg.MaxBody()),
+			strconv.Itoa(seg.SumBody()),
+			strconv.FormatFloat(seg.AvgBody(), 'f', 3, 64),
+		})
+	}
+
+	return htmlTemplate.Execute(w, data)
+}
+
+var htmlTemplate = template.Must(template.New("accessprof").Parse(`<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.datatables.net/t/bs-3.3.6/jqc-1.12.0,dt-1.10.11/datatables.min.css"/>
+    <script src="https://cdn.datatables.net/t/bs-3.3.6/jqc-1.12.0,dt-1.10.11/datatables.min.js"></script>
+    <script>
+      jQuery(function($){
+        $("#foo-table").DataTable();
+      });
+    </script>
+    <title></title>
+  </head>
+  <body>
+    <table id="foo-table" class="table table-bordered">
+      <thead>
+        <tr>
+          {{ range .Header }}
+            <th>{{.}}</th>
+          {{ end }}
+        </tr>
+      </thead>
+      <tbody>
+        {{ range .Rows }}
+          <tr>
+            {{ range . }}
+              <td>{{ . }}</td>
+            {{end}}
+          </tr>
+        {{ end }}
+      </tbody>
+    </table>
+  </body>
+</html>
+`))
