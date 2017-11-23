6b35a14dce828b405dc3a3d4fa8e18882b075765
agatan <agatan039@gmail.com>
2017-11-23 17:57:16 +0900

    Stop doint example test

---
+++ _example/example.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+
+	"github.com/agatan/accessprof"
+)
+
+var exampleHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Path: %s\n", r.URL.Path)))
+	io.Copy(w, r.Body)
+	r.Body.Close()
+})
+
+func main() {
+	var a accessprof.AccessProf
+	server := httptest.NewServer(a.Wrap(exampleHandler))
+	defer server.Close()
+
+	http.Get(server.URL)
+	http.Get(server.URL + "/test")
+	http.Get(server.URL + "/test")
+	http.Post(server.URL, "application/json", strings.NewReader("{}"))
+	http.Post(server.URL, "application/json", strings.NewReader(`{"key": "value"}`))
+
+	report := a.Report()
+	fmt.Print(report.String())
+
+	// Output:
+	// +--------+--------+-------+-------+---------+----------+----------+----------+-----------+-----------+-----------+-----------+
+	// | STATUS | METHOD | PATH  | COUNT |   MIN   |   MAX    |   SUM    |   AVG    | MIN(BODY) | MAX(BODY) | SUM(BODY) | AVG(BODY) |
+	// +--------+--------+-------+-------+---------+----------+----------+----------+-----------+-----------+-----------+-----------+
+	// |    200 | GET    | /     |     1 | 2.246µs | 2.246µs  | 2.246µs  | 2.246µs  |         8 |         8 |         8 |     8.000 |
+	// |    200 | GET    | /test |     2 | 1.858µs | 2.077µs  | 3.935µs  | 1.967µs  |        12 |        12 |        24 |    12.000 |
+	// |    200 | POST   | /     |     2 | 9.709µs | 10.463µs | 20.172µs | 10.086µs |        10 |        24 |        34 |    17.000 |
+	// +--------+--------+-------+-------+---------+----------+----------+----------+-----------+-----------+-----------+-----------+
+}
--- accessprof_test.go
+++ accessprof_test.go
@@ -58,28 +58,3 @@ func TestAccessProf_Report_aggregatesByPath(t *testing.T) {
 		t.Fatalf("expected 2 report segments, GET / and GET /test; but got %d", len(report.Segments))
 	}
 }
-
-func ExampleAccessProf() {
-	var a AccessProf
-	server := httptest.NewServer(a.Wrap(testHandler))
-	defer server.Close()
-
-	http.Get(server.URL)
-	http.Get(server.URL + "/test")
-	http.Get(server.URL + "/test")
-	http.Post(server.URL, "application/json", strings.NewReader("{}"))
-	http.Post(server.URL, "application/json", strings.NewReader(`{"key": "value"}`))
-
-	report := a.Report()
-	fmt.Println(report.String())
-
-	// Output:
-	// +--------+--------+-------+-------+---------+----------+----------+----------+-----------+-----------+-----------+-----------+
-	// | STATUS | METHOD | PATH  | COUNT |   MIN   |   MAX    |   SUM    |   AVG    | MIN(BODY) | MAX(BODY) | SUM(BODY) | AVG(BODY) |
-	// +--------+--------+-------+-------+---------+----------+----------+----------+-----------+-----------+-----------+-----------+
-	// |    200 | GET    | /     |     1 | 2.246µs | 2.246µs  | 2.246µs  | 2.246µs  |         8 |         8 |         8 |     8.000 |
-	// |    200 | GET    | /test |     2 | 1.858µs | 2.077µs  | 3.935µs  | 1.967µs  |        12 |        12 |        24 |    12.000 |
-	// |    200 | POST   | /     |     2 | 9.709µs | 10.463µs | 20.172µs | 10.086µs |        10 |        24 |        34 |    17.000 |
-	// +--------+--------+-------+-------+---------+----------+----------+----------+-----------+-----------+-----------+-----------+
-
-}
